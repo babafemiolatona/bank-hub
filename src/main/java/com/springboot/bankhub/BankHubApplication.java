f6868da495587ff594fd01c3e3c9f1b4bd57caff
babafemiolatona <femiolatona@gmail.com>
2024-09-02 17:33:54 +0100

    Initial commit

---
+++ src/main/java/com/springboot/bankhub/BankHubApplication.java
@@ -0,0 +1,21 @@
+package com.springboot.bankhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BankHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankHubApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+
+}
---
+++ src/test/java/com/springboot/bank_hub/BankHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bank_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
